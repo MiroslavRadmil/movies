e275dd16fb6ecebd8eb777f31cf3a177f5711494
Miroslav Radmil <Mirek1913@gmail.com>
2023-04-27 18:30:08 +0200

    Načtení základních souborů

---
+++ src/components/Modal.js
@@ -0,0 +1,20 @@
+import "./Modal.css"
+import { useEffect } from "react"
+
+
+const Modal = ({notContenet, closeNotif}) => {
+
+  useEffect( () => {
+    setTimeout( () => {
+        closeNotif()
+    }, 2000)
+  })
+
+  
+  return (
+    <div>{notContenet}</div>
+  )
+}
+
+
+export default Modal
