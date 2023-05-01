1bcfae4595f9d3170cc31535759f368b03f50a07
Miroslav Radmil <Mirek1913@gmail.com>
2023-05-01 18:49:32 +0200

    vložení kódu s use-reducer vytvořeného dříve

--- src/pages/serials.js
+++ src/pages/serials.js
@@ -1,72 +1,128 @@
-import { useState } from "react"
+import Modal from "../components/Modal"
+
+import { useState, useReducer } from "react"
+
+
+
+
+
+const reducer = (state, action) => {
+  
+
+
+  if (action.type === "ADD_MOVIE"){
+    const newMovies = [...state.movies, action.payload]
+    console.log(state.movies);
+      return {
+        ...state,
+        movies: newMovies,
+        showNotification: true,
+        notificationContent: "Film byl přidán"
+      }
+  }
+
+  if (action.type === "EMPTY_MOVIE"){
+    return {
+      ...state,
+      showNotification: true,
+      notificationContent: "zadej film"
+    }
+  }
+
+  if (action.type === "CLOSE_NOTIFICATION"){
+    return {
+      ...state,
+      showNotification: false
+    }
+  }
+
+  if (action.type === "REMOVE"){
+    const filteredMovies = state.movies.filter( (oneMovie) => {
+      return oneMovie.id !== action.payload
+    })
+ 
+    return {
+      ...state,
+      movies: filteredMovies
+    }
+  }
+
+
+
+  return new Error()
+}
+
+
+const defaultState = {
+    movies: [],
+    showNotification: false,
+    notificationContent: "",
+    classN: ""
+}
 
 
 
 
 const Serials = () => {
-
-  const [data, setData] = useState([])
-
-  const [serialTitle, setSerialTitle] = useState("")
-  const [score, setScore] = useState(5)
+  
+  const [movieName, setMovieName] = useState("")
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
 
+ 
 
-  const submitSerialsForm = (e) => {
+
+
+  const submitForm = (e) => {
     e.preventDefault()
 
-    const newSerial = {serialTitle: serialTitle, score: score} 
-    
-    if (serialTitle !== "") {
-    
-    data.push({...newSerial.data()})
-    setScore("5")
-    setSerialTitle("")}
-    else {
-      setData([])
+
+    if (movieName) {
+      const newMovie = {id: new Date().getTime(), name: movieName}
+      dispatch({type: "ADD_MOVIE", payload: newMovie})
+    } else {
+      dispatch ( {type: "EMPTY_MOVIE"})
     }
-  }
-  
 
+    setMovieName("")
 
-
-  return (
-   <div className="all-serials">
-   <h2>Přidej a ohodnoť svůj oblíbený seriál</h2>
-
-   <form className="add-serials-form" onSubmit={submitSerialsForm}>
-      <input type="text" 
-             placeholder="Název seriálu" 
-             onChange={(e) => setSerialTitle(e.target.value) } 
-             required 
-             value={serialTitle}/>
-      <input type="number"
-             placeholder="Oblíbenost 1-10"
-             onChange={(e) => setScore(e.target.value)}
-             min="1"
-             max="10"
-             value={score}>
-      </input>
-
-      <input type="submit" value="Přidej" />
-    </form>
-
-    {data.map( (oneSerial, index) => {
-        const {serialTitle, score} = oneSerial
-       
-          
-        return <div key={index} className="one-serial">
-         {<p> {serialTitle}</p>}
-        
-                 
-
-      <input type="number" value={score}></input>
-      </div>
-
-    })}
-     
-  </div>
-  )
+    
 }
+
+
+
+  const closeNotification = () => {
+    dispatch ({type: "CLOSE_NOTIFICATION"})
+  }
+
+
+
+  return <section>
+    {state.showNotification && <Modal notContenet={state.notificationContent} closeNotif={closeNotification}/>}
+    <form onSubmit={submitForm}>
+      <input type="text" onChange={ (e) => setMovieName(e.target.value) } value={movieName}/>
+      <input type="submit" value="Přidat" />
+    </form>
+    <div>
+      {state.movies.map( (oneMovie) => {
+        return <div key={oneMovie.id}>
+          <p>{oneMovie.name}</p>
+          <button onClick={() => dispatch ({type: "REMOVE", payload: oneMovie.id })}>
+            Smaž
+          </button>
+
+
+
+
+           
+          
+         
+
+        </div>
+      } )}
+    </div>
+  </section>
+}
+
 
 export default Serials
